1c56ce95d946c946da25eb48d8ab09992a230b54
NhatTan <nguyenhoangnhattan417@gmail.com>
2023-10-27 16:37:52 +0700

    27/10/23: Modified datasets, basic preprocessing, data analysis

---
+++ baseline/211023_NhatTan_Test/constants.py
@@ -0,0 +1,15 @@
+import emoji
+
+
+ALL_EMOJI = list(emoji.EMOJI_DATA.keys())
+ALL_EMOJI.append("♡")
+ALL_EMOJI.append("★")
+ALL_EMOJI.append("=)")  # TODO: HANDLE LONGER CASE =))))))
+ALL_EMOJI.append(":)")  # TODO: HANDLE LONGER CASE :))))))
+ALL_EMOJI.append("^ _ ^")
+# ALL_EMOJI.append("^^")
+ALL_EMOJI.append("⚘")
+ALL_EMOJI.append("☆")
+
+
+VI_ALL = 'AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXYÁÀẢÃẠẮẰẲẴẶẤẦẨẪẬÉÈẺẼẸẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌỐỒỔỖỘỚỜỞỠỢÚÙỦŨỤỨỪỬỮỰÝỲỶỸỴaăâbcdđeêghiklmnoôơpqrstuưvxyáàảãạắằẳẵặấầẩẫậéèẻẽẹếềểễệíìỉĩịóòỏõọốồổỗộớờởỡợúùủũụứừửữựýỳỷỹỵ'
