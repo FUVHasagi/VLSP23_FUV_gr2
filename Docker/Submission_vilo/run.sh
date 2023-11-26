f1a34936ddaa7e348417aa31593fe1c2e7c3f24a
kenha212 <95837348+kenha212@users.noreply.github.com>
2023-11-26 13:58:23 +0700

    Add files via upload

---
+++ Docker/Submission_vilo/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Prompt the usr for the input file path
+INPUT_TEXT_FILE=$1
+echo "Enter the path to your input file: $1"
+
+
+
+# Check if the provided file exists
+if [ ! -f "$INPUT_TEXT_FILE" ]; then
+    echo "Error: File not found at $INPUT_TEXT_FILE"
+    exit 1
+fi
+
+python /app/MT-Preparation/subwording/2-subword.py /app/source_19.model /app/target_19.model "$INPUT_TEXT_FILE" /app/split_val2023_10kto100k.lo
+
+INPUT_TEXT_FILE+=".subword"
+
+# Run the translation and evaluation commands with GPU if available, otherwise use CPU
+if [ -x /usr/bin/nvidia-smi ]; then
+    onmt_translate -model /app/model.vilo_avg8085.pt \
+    -src "$INPUT_TEXT_FILE" \
+    -output /app/UN.lo.translated -gpu 0 -min_length 1
+else
+    onmt_translate -model /app/model.vilo_avg8085.pt \
+    -src "$INPUT_TEXT_FILE" \
+    -output /app/UN.lo.translated -min_length 1
+fi
+
+python /app/MT-Preparation/subwording/3-desubword.py /app/target_19.model /app/UN.lo.translated
